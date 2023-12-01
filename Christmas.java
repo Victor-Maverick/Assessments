ea984d9f8bd1a3e22fcf90ac7f700d8c496bd652
Victor-Maverick <151345787+Victor-Maverick@users.noreply.github.com>
2023-12-01 03:07:26 +0100

    Add files via upload

--- Christmas.java
+++ Christmas.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 
   System.out.println("Enter a day of Christmas: ");
   int dayOfChristmas = scanner.nextInt();
-
+  
   switch(dayOfChristmas / 1){
   case 1:
    System.out.println("On the first day of Christmas my true love sent to me ");
@@ -46,46 +46,35 @@ import java.util.Scanner;
    System.out.println("On the twelfth day of Christmas my true love sent to me ");
   }
 
-System.out.println("Enter the day number of Christmas for your gift: ");
-  int giftOfChristmas = scanner.nextInt();
-
-  switch(giftOfChristmas / 1){
-  case 1:
-   System.out.println("A partridge in a pear tree");
-   break;
-  case 2:
-   System.out.println("Two turtle doves");
-    break;
-  case 3:
-   System.out.println("Three French hens");
-    break;
-  case 4:
-   System.out.println("Four calling birds");
-    break;
-  case 5:
-   System.out.println("Five Golden rings");
-    break;
-  case 6:
-   System.out.println("Six geese a-laying");
-    break;
-  case 7:
-   System.out.println("Seven swans a-swimming");
-   break;
-  case 8:
-   System.out.println("Eight maids a making");
-     break;
-  case 9: 
-  System.out.println("Nine ladies dancing");
-    break;
-  case 10: 
-  System.out.println("10 lords a-leaping");
-     break;
-  case 11:
-   System.out.println("11 pipers piping");
-  break;
+  switch(dayOfChristmas / 1){
+  
   case 12:
    System.out.println("12 drummers drumming");
-  }
+   case 11:
+    System.out.println("11 pipers piping");
+   case 10: 
+    System.out.println("10 lords a-leaping");
+   case 9: 
+    System.out.println("Nine ladies dancing");
+   case 8:
+    System.out.println("Eight maids a making");
+    case 7:
+   System.out.println("Seven swans a-swimming");
+    case 6:
+   System.out.println("Six geese a-laying");
+    case 5:
+   System.out.println("Five Golden rings");
+    case 4:
+   System.out.println("Four calling birds");
+    case 3:
+   System.out.println("Three French hens");
+    case 2:
+   System.out.println("Two turtle doves");
+   case 1:
+   System.out.println("A partridge in a pear tree");
+   break;
+  
+   }
 
 
   }
